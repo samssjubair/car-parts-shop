5dedadd7762130489f5cf64db56cb0e376aace7e
samssjubair <samssjubair@gmail.com>
2023-04-20 18:11:17 +0600

    form style change

--- client/pages/components/CarFormDocuments.jsx
+++ client/pages/components/CarFormDocuments.jsx
@@ -76,10 +76,143 @@ const CarFormDocuments = () => {
     });
   }, []);
 
+  const [brandName, setBrandName] = useState("");
+  const [productionYear, setProductionYear] = useState("");
+  const [modelName, setModelName] = useState("");
+  const [requiredParts, setRequiredParts] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [deliveryAddress, setDeliveryAddress] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle form submission logic here
+    console.log(event);
+  };
+
+
   return (
     <div>
       <div className="flex justify-center home-input-field-area my-8">
-          <div className="bg-gray-900 home-input-field">
+      <div class="w-1/2 bg-white rounded-lg shadow dark:border md:mt-0  xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+  <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+
+    <form class="space-y-4 md:space-y-6" action="#">
+      <div>
+        <label for="brand-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Brand Name</label>
+        <input value={brand} onChange={onBrandChange} type="text" name="brand-name" id="brand-name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter brand name" required=""/>
+        <div className="dropdown">
+          {allBrand
+            .filter((item) => {
+              const searchTerm = brand.toLowerCase();
+              const fullName = item.toLowerCase();
+
+              return (
+                searchTerm &&
+                fullName.startsWith(searchTerm) &&
+                fullName !== searchTerm
+              );
+            })
+            .slice(0, 10)
+            .map((item,index) => (
+              <div
+                onClick={() => setBrand(item)}
+                className="dropdown-row"
+                key={index}
+              >
+                {item}
+              </div>
+            ))}
+        </div>
+      </div>
+      
+      <div>
+        <label for="year" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Year</label>
+        <input value={year} onChange={onYearChange} type="number" name="year" id="year" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter year" required=""/>
+        <div className="dropdown">
+        {allYear.filter((item) => {
+            const searchTerm = year.toString().toLowerCase();
+            const fullName = item.toString().toLowerCase();
+            // console.log(searchTerm,fullName)
+
+            return (
+              searchTerm &&
+              fullName.startsWith(searchTerm) &&
+              fullName !== searchTerm
+            );
+          })
+          .slice(0, 5)
+          .map((item,index) => (
+            <div
+              onClick={() => setYear(item)}
+              className="dropdown-row"
+              key={index}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <label for="model-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Model Name</label>
+        <input value={model} onChange={onModelChange} type="text"  name="model-name" id="model-name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter model name" required=""/>
+        <div className="dropdown">
+        {allModelName.filter((item) => {
+            const searchTerm = model.toLowerCase();
+            const fullName = item.toLowerCase();
+            // console.log(searchTerm,fullName)
+
+            return (
+              searchTerm &&
+              fullName.startsWith(searchTerm) &&
+              fullName !== searchTerm
+            );
+          })
+          .slice(0, 5)
+          .map((item,index) => (
+            <div
+              onClick={() => setModel(item)}
+              className="dropdown-row"
+              key={index}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <label for="parts-required" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Parts Required</label>
+        <input type="text" name="parts-required" id="parts-required" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter parts required" required=""/>
+      </div>
+      <div>
+        <label for="quantity" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantity</label>
+        <input type="number" name="quantity" id="quantity" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter quantity" required=""/>
+      </div>
+      <div>
+          <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+          <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Doe" required=""/>
+      </div>
+      <div>
+          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+          <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+      </div>
+      <div>
+          <label for="mobile" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mobile</label>
+          <input type="tel" name="mobile" id="mobile" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="+1 555-555-5555" required=""/>
+      </div>
+      <div>
+          <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
+          <textarea name="address" id="address" rows="3" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123 Main St, Anytown USA" required=""></textarea>
+      </div>
+      <div class="flex flex-col items-center">
+        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Submit</button>
+      </div>
+      </form>
+      </div>
+      </div>
+          {/* <div className="bg-gray-900 home-input-field">
             <form className="px-6 ">
               <div className="space-y-12 ">
                 <div className="border-b border-gray-900/10 pb-12">
@@ -325,7 +458,7 @@ const CarFormDocuments = () => {
                 </button>
               </div>
             </form>
-          </div>
+          </div> */}
         </div>
     </div>
   )
